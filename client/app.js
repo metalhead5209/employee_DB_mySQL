262bd464450e3f1ee30b722a687cbd6d3560b9d5
Aaron Rodi <metalhead5209@gmail.com>
2022-06-11 13:45:00 -0500

    more logic

--- client/app.js
+++ client/app.js
@@ -9,17 +9,21 @@ const addEmpBtn = document.querySelector('#addBtn').addEventListener('click', ()
     const nameInput = document.querySelector('#emp');
     const name = nameInput.value;
     nameInput.value = '';
+    if(!name) {
+        alert('Please Enter Name')
+    } else {
 
+    
     fetch('http://localhost:5252/add', {
         headers: {
             'Content-type': 'application/json'
         },
         method: 'post',
-        body: JSON.stringify({ name : name})
+        body: JSON.stringify({ name : name })
     })
     .then(res => res.json())
     .then(data => rowInTable(data['data']));
-    console.log(name)
+    window.location.reload();}
 })
     
 const rowInTable = (data) => {
@@ -29,11 +33,21 @@ const rowInTable = (data) => {
 const loadTable = (data) => {
     const table = document.querySelector('table tbody');
     
-    // console.log(data);
-    
-
     if (data.length === 0) {
         table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+        return;
     }
-}
+    let tableHtml = '';
 
+    data.forEach(({id, name, date_hired}) => {
+        tableHtml += '<tr>';
+        tableHtml += `<td>${id}</td>`;
+        tableHtml += `<td>${name}</td>`;
+        tableHtml += `<td>${new Date(date_hired).toLocaleString()}</td>`;
+        tableHtml += `<td><button class="delete-row-btn" data-id=${id}>Delete</td>`;
+        tableHtml += `<td><button class="edit-row-btn" data-id=${id}>Edit</td>`;
+        tableHtml += '</tr>';
+    });
+
+    table.innerHTML = tableHtml;
+}
